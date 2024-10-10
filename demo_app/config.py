884b3838c15f72fa63adb2f44ff9d5ac20aaf7ee
Yerco Jorquera <yjorq@spectrumai.com>
2024-10-10 10:36:50 -0300

    Disablling redis on CQRS by default

--- demo_app/config.py
+++ demo_app/config.py
@@ -5,7 +5,7 @@ config = {
     'BASE_DIR': base_dir,
     'SECRET_KEY': 'supersecretkey',
     'DATABASE_URL': 'sqlite+aiosqlite:///demo_app.db',
-    'USE_REDIS_FOR_CQRS': True,
+    'USE_REDIS_FOR_CQRS': False,
     'SESSION_EXPIRY_SECONDS': 7200,  # Extend session expiry to 2 hours
     'TEMPLATE_ENGINE': 'JinjaAdapter',
     'TEMPLATE_DIR': os.path.join(base_dir, 'templates'),
